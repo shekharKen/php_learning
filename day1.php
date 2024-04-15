161d213dd1813ffa0b121b178c52bc616b31a82a
Shekhar <shekhar@NexGs-MacBook-Pro.local>
2024-04-15 23:03:26 +0530

    day 1

---
+++ day1.php
@@ -0,0 +1,51 @@
+<h2>abc</h2>
+
+
+<?php
+    // PHP Tag 
+
+    // variables syntax
+    $abc;
+
+    // datatypes
+    // numeric values = 1, 1000, 999, 4545 = integers
+    // doubles / FLoats = 1.345, 345.5677
+    // strings  = "abcstring213412", "$%%sxfsadf", 'asdfsdf',
+    // array = [], array();
+    // objects = class on instance 
+
+
+
+    $x = 1234; // int
+    $str = "stringabc asdf"; // string
+    $f = 12.56; // float
+
+
+    // normal echo
+    echo "<h1>H! Tag</h1>";
+    echo "\n";
+
+    // variable
+    echo $x;
+
+    echo "\n";
+
+    echo $str;
+
+    // print 
+    print($f);
+    print('<h1>H! Tag</h1>');
+
+
+
+
+
+// closing tag
+?>
+
+
+<h2>
+<?php
+ echo $str;
+?>
+</h2>
